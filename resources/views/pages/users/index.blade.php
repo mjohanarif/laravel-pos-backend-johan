a31feb3c3b154246822243e1a961960024102e2d
mjohanarif <m.johanarifin@gmail.com>
2023-12-15 20:03:02 +0700

    feat user crud management

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -13,7 +13,7 @@
             <div class="section-header">
                 <h1>Users</h1>
                 <div class="section-header-button">
-                    <a href="features-post-create.html" class="btn btn-primary">Add New</a>
+                    <a href="{{ route('user.create') }}" class="btn btn-primary">Add New</a>
                 </div>
                 <div class="section-header-breadcrumb">
                     <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
@@ -22,6 +22,11 @@
                 </div>
             </div>
             <div class="section-body">
+                <div class="row">
+                    <div class="col-12">
+                        @include('layouts.alert')
+                    </div>
+                </div>
                 <h2 class="section-title">Users</h2>
                 <p class="section-lead">
                     You can manage all users, such as editing, deleting and more.
@@ -74,7 +79,24 @@
                                                 </td>
                                                 <td>{{ $user->created_at }}</td>
                                                 <td>
-                                                    <div class="badge badge-primary">Published</div>
+                                                    <div class="d-flex justify-content-center">
+                                                        <a href="{{ route('user.edit', $user->id) }}"
+                                                            class="btn btn-sm btn-info btn-icon">
+                                                            <i class="fas fa-edit"></i>
+                                                            Edit
+                                                        </a>
+
+                                                        <form action="{{ route('user.destroy', $user->id) }}" method="POST"
+                                                            class="ml-2">
+                                                            <input type="hidden" name="_method" value="DELETE" />
+                                                            <input type="hidden" name="_token"
+                                                                value="{{ csrf_token() }}" />
+                                                            <button class="btn btn-sn btn-danger btn-icon confirm-delete">
+                                                                <i class="fas fa-times"></i> Delete
+                                                            </button>
+                                                        </form>
+
+                                                    </div>
                                                 </td>
                                             </tr>
                                         @endforeach
